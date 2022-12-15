f78106beb1a03cc619182505c59d0e52de38735f
Rasu1ova <rassurayyo9898@gmail.com>
2022-12-15 12:40:33 +0500

    test

---
+++ src/components/new.jsx
@@ -0,0 +1,9 @@
+function News() {
+    return (
+      <div className="App">
+        <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem, nemo?</h1>
+      </div>
+    );
+  }
+  
+  export default News;
