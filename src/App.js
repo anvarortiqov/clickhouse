f6fbb5cb1d1f01400aa5580570accc6c6c008884
anvarortiqov <anvar.ortiqov98@gmail.com>
2022-12-15 11:29:34 +0500

    Creat Project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem, nemo?</h1>
+    </div>
+  );
+}
+
+export default App;
